70c199a14aac8d7eeeeecc606aa3cfc41707efeb
AngelinaPierre <gaptp95@gmail.com>
2023-05-29 15:38:24 -0300

    app complited

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
+
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import clientPromise from '../lib/mongodb'
 
 export default function Home({ isConnected }) {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const results = await fetch("/api/list");
+      const resultsJson = await results.json();
+      setRestaurants(resultsJson);
+    })();
+  }, []);
   return (
     <div className="container">
       <Head>
@@ -13,37 +24,13 @@ export default function Home({ isConnected }) {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
         </h1>
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '} for instructions
-          </h2>
-        )}
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className="grid">
-          <a href="https://nextjs.org/docs" className='card'>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-          <a href="https://nextjs.org/learn" className='card'>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerpate example Next.js projects.</p>
-          </a>
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className='card'>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {restaurants.map(r => (
+            <div className="card" key={r._id}>
+              <h2>{r.name}</h2>
+              <p>{r.cuisine}</p>
+            </div>
+          ))}
         </div>
       </main>
 
@@ -187,6 +174,7 @@ export default function Home({ isConnected }) {
   )
 }
 
+// serverless function = `getServerSideProps` will run serverside on every render
 
 export async function getServerSideProps(context) {
   try {
